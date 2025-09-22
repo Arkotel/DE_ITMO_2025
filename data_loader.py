bf9794e760849ef92f57021231b8fa77742e9dad
Arkotel <arkotelmk@gmail.com>
2025-09-22 22:03:33 +0300

    Print the csv file correctly

--- data_loader.py
+++ data_loader.py
@@ -3,6 +3,6 @@ import pandas as pd
 FILE_ID = "1gJrXyvqIVSZCEjqhGhvisyMyxI0zBald"  # ID файла на Google Drive
 file_url = f"https://drive.google.com/uc?id={FILE_ID}"
 
-raw_data = pd.read_csv(file_url)  # читаем файл
+raw_data = pd.read_csv(file_url, sep=";")  # читаем файл
 
 print(raw_data.head(10))  # выводим на экран первые 10 строк для проверки
