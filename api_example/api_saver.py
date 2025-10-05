3ef15a351f84fe9a0d1fa61ee0816a6067b7ed12
Arkotel <arkotelmk@gmail.com>
2025-10-06 00:45:28 +0300

    Add parquet export

--- api_example/api_saver.py
+++ api_example/api_saver.py
@@ -4,7 +4,8 @@ import os
 m_dir = os.path.dirname(os.path.abspath(__file__))
 API_DATA = os.path.join(m_dir, "api_data")
 API_IMAGES = os.path.join(m_dir, "api_images")
-OUTPUT_FILENAME = os.path.join(API_DATA, "disney_characters.csv")
+OUTPUT_CSV = os.path.join(API_DATA, "disney_characters.csv")
+OUTPUT_PARQUET = os.path.join(API_DATA, "disney_characters.parquet")
 
 
 def api_to_csv(data: list[dict]) -> pd.DataFrame | None:
@@ -16,6 +17,21 @@ def api_to_csv(data: list[dict]) -> pd.DataFrame | None:
     # Создаем папку если ее нет
     os.makedirs(API_DATA, exist_ok=True)
 
+    raw_api_dataset = pd.DataFrame(data)
+    raw_api_dataset.to_csv(OUTPUT_CSV, index=False)
+    print(f"\nДанные сохранены в {OUTPUT_CSV}")
+
+    return raw_api_dataset
+
+
+def api_to_parquet(data: list[dict]) -> pd.DataFrame | None:
+    """Сохраняет типизированные данные в Parquet"""
+    if not data:
+        print("Ошибка. Данные отсутствуют")
+        return None
+
+    os.makedirs(API_DATA, exist_ok=True)
+
     api_dataset = pd.DataFrame(data)
 
     if "_id" in api_dataset.columns:
@@ -24,7 +40,9 @@ def api_to_csv(data: list[dict]) -> pd.DataFrame | None:
     if "name" in api_dataset.columns:
         api_dataset["name"] = api_dataset["name"].astype("category")
 
-    api_dataset.to_csv(OUTPUT_FILENAME, index=False)
-    print(f"Данные сохранены в {OUTPUT_FILENAME}")
+    api_dataset.to_parquet(OUTPUT_PARQUET, index=False)
+    print(
+        f"\nОбработанные данные сохранены в {os.path.basename(OUTPUT_PARQUET)}"
+    )  # noqa
 
     return api_dataset
