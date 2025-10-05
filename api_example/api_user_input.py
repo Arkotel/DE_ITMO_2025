1025e80f246d9606a86d33d184d9cc3c2d9a3dd7
Arkotel <arkotelmk@gmail.com>
2025-10-06 00:48:08 +0300

    Add COUNT_MAX env var

--- api_example/api_user_input.py
+++ api_example/api_user_input.py
@@ -1,3 +1,8 @@
+import os
+
+count_max_str = os.environ.get("COUNT_MAX")
+count_max = int(count_max_str)
+
 """Запрашиваем у пользователя количество персонажей для загрузки"""
 
 
@@ -10,8 +15,11 @@ def get_character_count() -> int:
             if count <= 0:
                 print("Ошибка. Число должно быть больше 0")
                 return 0
-            elif count > 10:
-                print("Ошибка. Макс. число персонажей для загрузки - 10")
+            elif count > count_max:
+                print(
+                    "Ошибка. Макс. число персонажей для загрузки - "
+                    + str(count_max_str)
+                )
                 return 0
             else:
                 # Если число в допустимом диапазоне
